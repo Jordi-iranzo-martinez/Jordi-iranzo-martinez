ca9246a3224878b9153f7e4653306905e1ea9fd0
Jordi Iranzo Martínez <jordi.im@proton.me>
2025-07-20 17:54:28 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,7 +21,9 @@ const config: QuartzConfig = {
     defaultDateType: "modified",
 theme: {
   lightMode: "light",
-  darkMode: "dark"
+  darkMode: "dark",
+  cdnCaching: true
+  },
 },
     head: [
       {
