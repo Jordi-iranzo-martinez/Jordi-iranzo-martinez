292f8e486437fea07b63ad19a4d40e77e33e4ba8
Jordi Iranzo Martínez <jordi.im@proton.me>
2025-07-20 13:33:32 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://jordi-iranzo-martinez.netlify.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
