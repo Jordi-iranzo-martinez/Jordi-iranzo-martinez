b58ab39d0e9941a49db50188ed43d55c021f4b0d
Jordi Iranzo Martínez <jordi.im@proton.me>
2025-07-20 17:58:44 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -2,95 +2,80 @@ import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 /**
- * Quartz 4 Configuration
- *
- * See https://quartz.jzhao.xyz/configuration for more information.
- */
+* Quartz 4 Configuration
+*
+* See https://quartz.jzhao.xyz/configuration for more information.
+*/
 const config: QuartzConfig = {
-  configuration: {
-    pageTitle: "Jordi Iranzo Martínez",
-    pageTitleSuffix: "",
-    enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "https://jordi-iranzo-martinez.netlify.app",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
-theme: {
-  lightMode: "light",
-  darkMode: "dark",
-  cdnCaching: true
-  },
-},
-    head: [
-      {
-        tag: "script",
-        props: {},
-        content: `
-          (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-          .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-          n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-          (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-          ml('account', '1647761');
-        `
-      }
-    ],
-  },
-head: [
-  {
-    tag: "script",
-    props: {},
-    content: `
-      (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-      .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-      n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-      (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-      ml('account', '1647761');
-    `
-  }
-],
-  plugins: {
-    transformers: [
-      Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
-      }),
-      Plugin.SyntaxHighlighting({
-        theme: {
-          light: "github-light",
-          dark: "github-dark",
-        },
-        keepBackground: false,
-      }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
-    ],
-    filters: [Plugin.RemoveDrafts()],
-    emitters: [
-      Plugin.AliasRedirects(),
-      Plugin.ComponentResources(),
-      Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
-      Plugin.ContentIndex({
-        enableSiteMap: true,
-        enableRSS: true,
-      }),
-      Plugin.Assets(),
-      Plugin.Static(),
-      Plugin.Favicon(),
-      Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
-    ],
-  },
+ configuration: {
+   pageTitle: "Jordi Iranzo Martínez",
+   pageTitleSuffix: "",
+   enableSPA: true,
+   enablePopovers: true,
+   analytics: {
+     provider: "plausible",
+   },
+   locale: "en-US",
+   baseUrl: "https://jordi-iranzo-martinez.netlify.app",
+   ignorePatterns: ["private", "templates", ".obsidian"],
+   defaultDateType: "modified",
+   head: [
+     {
+       tag: "script",
+       props: {},
+       content: `
+         (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+         .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+         n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+         (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+         ml('account', '1647761');
+       `
+     }
+   ],
+ },
+ theme: {
+   lightMode: "light",
+   darkMode: "dark",
+   cdnCaching: true
+ },
+ plugins: {
+   transformers: [
+     Plugin.FrontMatter(),
+     Plugin.CreatedModifiedDate({
+       priority: ["frontmatter", "git", "filesystem"],
+     }),
+     Plugin.SyntaxHighlighting({
+       theme: {
+         light: "github-light",
+         dark: "github-dark",
+       },
+       keepBackground: false,
+     }),
+     Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+     Plugin.GitHubFlavoredMarkdown(),
+     Plugin.TableOfContents(),
+     Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+     Plugin.Description(),
+     Plugin.Latex({ renderEngine: "katex" }),
+   ],
+   filters: [Plugin.RemoveDrafts()],
+   emitters: [
+     Plugin.AliasRedirects(),
+     Plugin.ComponentResources(),
+     Plugin.ContentPage(),
+     Plugin.FolderPage(),
+     Plugin.TagPage(),
+     Plugin.ContentIndex({
+       enableSiteMap: true,
+       enableRSS: true,
+     }),
+     Plugin.Assets(),
+     Plugin.Static(),
+     Plugin.Favicon(),
+     Plugin.NotFoundPage(),
+     Plugin.CustomOgImages(),
+   ],
+ },
 };
 
 export default config
