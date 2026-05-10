e5f75adeaa1009e7e2d4d40d1fc6855a27a954a4
Jordi Iranzo Martínez <jordi.im@proton.me>
2026-05-10 20:46:05 +0200

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,6 +40,7 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta name="follow.it-verification-code" content="44HGHCWjsubsl8GUYhW3" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
