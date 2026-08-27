480deebcdc3b3367878c05ff7748ffc8924109c7
Jordi Iranzo Martínez <jordi.im@proton.me>
2026-08-27 23:20:33 +0200

    Update quartz.config.ts

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Jordi Iranzo Martínez",
+    pageTitleSuffix: "Hola",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
