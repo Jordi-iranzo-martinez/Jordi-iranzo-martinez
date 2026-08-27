5047cab12602470f05478320ed7e646bd0081a4b
Jordi Iranzo Martínez <jordi.im@proton.me>
2026-08-27 23:23:32 +0200

    Update quartz.config.ts

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jordi.iranzo.martinez.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
